3d765f5e2976d35cdc0a8d81f701e1e48abeddde
Vanessa~Roux <vanessa.roux891@orange.fr>
2019-01-07 16:49:03 +0100

    Lancement projet & config Symfony

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sophro_NativeStage
+
+## Stage du 10 décembre 2018 au 22 février 2019 chez Native Web
+
+### Sujet : Refonte d'un site vitrine en site dynamique avec le framework php Symfony 
---
+++ sophrologueSite/src/Controller/AppController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('app/home.html.twig');
+    }
+
+    /**
+     * @Route("/app", name="app")
+     */
+    public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+
+
+}
