ef939f1eb1177b0b760b61e6407e0c8669dedb7b
Vanessa~Roux <vanessa.roux891@orange.fr>
2019-01-08 09:29:14 +0100

    création entity admin & visuel de base twig

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Stage du 10 décembre 2018 au 22 février 2019 chez Native Web
 
-### Sujet : Refonte d'un site vitrine en site dynamique avec le framework php Symfony 
+### Sujet : Refonte d'un site vitrine en site web dynamique en intégrant une interface de gestion Admin avec le framework php Symfony 
---
+++ sophrologueSite/src/Entity/Admin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdminRepository")
+ */
+class Admin
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     */
+    private $role;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(?string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
