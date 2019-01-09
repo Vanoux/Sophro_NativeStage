6fef622c7ce287b027204a2da35ffddea8a87525
Vanessa~Roux <vanessa.roux891@orange.fr>
2019-01-09 09:24:09 +0100

    fix bug

--- sophrologueSite/src/Migrations/Version20190108154954.php
+++ sophrologueSite/src/Migrations/Version20190108154954.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190108162539 extends AbstractMigration
+final class Version20190108154954 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE admin CHANGE role role TINYTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, mail VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(80) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20190108162539 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE admin CHANGE role role VARCHAR(80) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('DROP TABLE admin');
     }
 }
